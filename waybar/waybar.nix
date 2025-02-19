dfdf47911e476f40f6b0fee5cd4e95da7c6b1a48
yummyhomie <eleedee@outlook.com>
2025-02-19 12:44:52 -0700

    This is how I disappear (connect 2 network)

--- waybar/waybar.nix
+++ waybar/waybar.nix
@@ -82,6 +82,7 @@
           format-wifi = " {signalStrength}";
           format-ethernet = "";
           format-disconnected = "";
+          on-click = "gnome-terminal -e nmtui";
           tooltip-format = "Connected to {essid}\nIP: {ipaddr}\nStrength: {signalStrength}%";
           tooltip-format-ethernet = "IP: {ipaddr}\nInterface: {ifname}";
           tooltip-format-disconnected = "Disconnected";
