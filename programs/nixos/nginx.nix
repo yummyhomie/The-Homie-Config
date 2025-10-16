0e4af33dd257797baf2e1ce365e09da21153f87c
yummyhomie <eleedee@outlook.com>
2025-10-15 22:45:33 -0600

    In My Room

--- programs/nixos/nginx.nix
+++ programs/nixos/nginx.nix
@@ -73,6 +73,22 @@
         '';
       };
     }; 
+
+    # GRAFANA
+    virtualHosts."dashboard.eleedee.net" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+        proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+    };
   };
 
   # SSL/TLS
