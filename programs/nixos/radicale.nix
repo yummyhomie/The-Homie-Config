ca8c4aaaf9e3fef1e227284b9a327b45d6746c96
[L]ED <110116836+yummyhomie@users.noreply.github.com>
2025-11-26 15:30:28 -0700

    Fixing comments

--- programs/nixos/radicale.nix
+++ programs/nixos/radicale.nix
@@ -8,7 +8,7 @@
         htpasswd_filename = "/var/lib/radicale/users";            
         htpasswd_encryption = "bcrypt";
       };    
-                          
+
       server.hosts = [ "0.0.0.0:5232" "[::]:5232" ];
       storage.filesystem_folder = "/var/lib/radicale/collections";
       web = {                                                                                            
@@ -33,6 +33,5 @@
     "${python}/${pkgs.python3.sitePackages}";
 }
 
-
-# For updating/creating passwords:                                                               
+# For updating/creating users & passwords:                                                               
 # sudo -u radicale htpasswd -bB /var/lib/radicale/users <username> <newpassword>
