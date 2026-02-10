f64bddf899a893a8b6e4054c3424d5e813f7a5da
yummyhomie <eleedee@outlook.com>
2026-02-10 15:41:09 -0700

    Docker stuff

--- machines/dell/config.nix
+++ machines/dell/config.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ../../programs/nixos/docker.nix
     ../../programs/nixos/mine-server.nix
     ../../programs/nixos/immich.nix
   ];
@@ -91,6 +92,6 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
