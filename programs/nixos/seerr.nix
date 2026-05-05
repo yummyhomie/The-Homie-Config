7d50c9df1c51baccb3e9a8fbed2014c0877f4cd9
yummyhomie <eleedee@outlook.com>
2026-05-05 11:33:23 -0600

    hope this works

--- programs/nixos/seerr.nix
+++ programs/nixos/seerr.nix
@@ -1,5 +1,9 @@
 {
   services.seerr.enable = true;
 
+  systemd.services.seerr.environment = {
+    HOST = "0.0.0.0";
+  };
+
   networking.firewall.allowedTCPPorts = [ 5055 ]; # Default Seerr Web Port
 }
