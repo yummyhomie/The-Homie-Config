d5f9de6f28782a21a7af3fd429f4deb6febf2627
yummyhomie <eleedee@outlook.com>
2026-05-08 23:04:58 -0600

    Time to reach the stars

--- programs/nixos/immich.nix
+++ programs/nixos/immich.nix
@@ -3,9 +3,21 @@
     enable = true;
     host = "0.0.0.0";
     openFirewall = true;
-    machine-learning.enable = true;
     mediaLocation = "/Warehouse/Memories";
+    
+    machine-learning.enable = false;
+    accelerationDevices = [ "null" ];
+
+    settings = {
+      ffmpeg = {
+        threads = 1;         # Limit transcoding to 1 CPU thread
+        crf = 28;            # Higher = smaller file, less CPU (default is 23)
+        preset = "ultrafast"; # Fastest preset, lowest CPU usage
+     };
+    };
   };
+
+  hardware.graphics.enable = true;
 
   users.users.immich.extraGroups = [ "video" "render" ];
 }
