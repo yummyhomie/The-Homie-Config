5a5b2348fe10b3cae821cd0721a6ef350c78fc44
yummyhomie <eleedee@outlook.com>
2026-05-11 16:32:58 -0600

    Fixing!

--- programs/nixos/airvpn.nix
+++ programs/nixos/airvpn.nix
@@ -1,6 +1,32 @@
 {
-  networking.wg-quick.interfaces.airvpn = {
-    configFile = "/etc/vpn/AirVPN_AllServers.vpn";
-    autostart = false;
+  vpnNamespaces.AirVPN = {
+    enable = true;
+    wireguardConfigFile = "/etc/vpn/AirVPN_AllServers.conf";
+
+    accessibleFrom = [ "192.168.1.0/24" ];
+
+    portMappings = [{ from = 2283; to = 8080; }];
+
+    openVPNPorts = [{ port = 21353; protocol = "both"; }];
+  };
+
+  systemd.services.qbittorrent.vpnConfinement = {
+    enable = true;
+    vpnNamespace = "AirVPN";
+  };
+
+  services.qbittorrent = {
+    enable = true;
+    webuiPort = 8080;
+    torrentingPort = 21353;
+    serverConfig = {
+      "Preferences" = {
+        "WebUI\\Address" = "192.168.15.1";
+        "WebUI\\Port" = "8080";
+        "Connection\\PortRangeMin" = "21353";
+        "WebUI\\BanDuration" = "3600";
+        "WebUI\\MaxAuthenticationFailCount" = "0";
+      };
+    };
   };
 }
