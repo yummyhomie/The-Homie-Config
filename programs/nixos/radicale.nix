f305260dac01c014c8e8a5f8d49dcf1b59f73317
yummyhomie <eleedee@outlook.com>
2026-05-27 11:36:14 -0600

    raaaaaaaad

--- programs/nixos/radicale.nix
+++ programs/nixos/radicale.nix
@@ -11,7 +11,10 @@
       server.hosts = [ "0.0.0.0:5232" "[::]:5232" ];
       storage.filesystem_folder = "/var/lib/radicale/collections";
     };     
-  };                                                
+  };
+
+
+  networking.firewall.allowedTCPPorts = [ 5232 ];
 }
 
 # For updating/creating users & passwords:                                                               
