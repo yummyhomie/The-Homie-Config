452344bd896114b7b2c750c73c8d0f8614009ddf
yummyhomie <eleedee@outlook.com>
2026-06-17 12:37:24 -0600

    finally!

--- programs/nixos/adguard.nix
+++ programs/nixos/adguard.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   services.adguardhome = {
     enable = true;
@@ -5,8 +6,9 @@
     openFirewall = true;
     host = "0.0.0.0";
     port = 1914;            # Port for the web interface
+    allowDHCP = true;
   };
 
-  networking.firewall.allowedTCPPorts = [ 53 1914 ]; # 53 is used for DNS queries.
-  networking.firewall.allowedUDPPorts = [ 53 ];
+  networking.firewall.allowedTCPPorts = [ 53 1914 ]; # 53 is used for DNS queries. 
+  networking.firewall.allowedUDPPorts = [ 53 67 68 ]; # 67 is DHCP Requests, 68 for DHCP Responses.
 }
