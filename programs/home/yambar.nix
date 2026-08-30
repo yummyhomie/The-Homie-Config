686d2015b853df63110965f6ed10642f890ff254
yummyhomie <eleedee@outlook.com>
2026-08-30 00:33:32 -0600

    Burnin the midnight oil

--- programs/home/yambar.nix
+++ programs/home/yambar.nix
@@ -15,7 +15,7 @@
             clock = {
               date-format = "%b %d";
               time-format = "%I:%M";
-              content = { string.text = "{date} {time} | "; };
+              content = { string.text = "{date} {time} |"; };
             };
           }
           {
@@ -50,7 +50,52 @@
                         };
                       };
                     };
+                    "name == \"wlp3s0\"" = {
+                      map = { 
+                        default = { 
+                          string = { 
+                            text = " 󰖩 {ipv4} | "; 
+                            on-click = "kitty sudo nmtui";    
+                          };
+                        };
+                        conditions = {
+                          "state == \"down\"" = { string.text = ""; };
+                        };
+                      };
+                    };
                   };
+                };
+              };
+            };
+          }
+          {
+            pipewire = {
+              content = {
+                list = {
+                  items = [
+                    {
+                      map = {
+                        conditions = {
+                          "type == \"sink\"" = {
+                            ramp = {
+                              tag = "linear_volume";
+                              items = [
+                                { string.text = "󰕿 {linear_volume}% "; }
+                                { string.text = "󰖀 {linear_volume}% "; }
+                                { string.text = "󰕾 {linear_volume}% "; }
+                              ];
+                              on-click = {
+                                left = "kitty pulsemixer";
+                                right = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+                                wheel-up = "wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 2%+";
+                                wheel-down = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-";
+                              };
+                            };
+                          };
+                        };
+                      };
+                    }
+                  ];
                 };
               };
             };
@@ -71,6 +116,13 @@
             mem = {
               poll-interval = 2500;
               content = { string.text = " {percent_used}% "; };
+            };
+          }
+          {
+            battery = {
+              name = "BAT0";
+              poll-interval = 30000;
+              content = { string.text = "󰂁{capacity}% "; };
             };
           }
         ];
